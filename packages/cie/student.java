fdf5954af76bfab9f722099f6f70a75d311dc876
misbahriyaz20 <95582255+misbahriyaz20@users.noreply.github.com>
2022-03-27 23:42:12 +0530

    Create student.java

---
+++ packages/cie/student.java
@@ -0,0 +1,22 @@
+package  cie;
+import java.util.*;
+
+public class student{
+ Scanner sc=new  Scanner(System.in);
+public  String usn=" ";
+public String name=" ";
+public  int sem=0;
+
+public  void getdata(){
+
+System.out.println("Usn:>");
+usn=sc.next();
+System.out.println("Name:>");
+name=sc.next();
+System.out.println("Semester:>");
+sem=sc.nextInt();
+}
+
+
+
+}
